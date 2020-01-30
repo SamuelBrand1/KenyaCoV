35f8aa67477e6d3da87a05e5bb99521ea925184b
Samuel Brand <sam055@mac.com>
2020-01-30 11:33:57 +0000

    added to comments

--- src/regularjumps.jl
+++ src/regularjumps.jl
@@ -2,6 +2,17 @@
 For this code to work the 3-dim representation should
 be unpacked as a vector
 
+This is row major unpacked so
+first 1,...,n entries are susceptibles in urban areas 1,...,n
+then n+1,...,2n are exposeds in urban areas 1,...,n
+.
+.
+.
+then n*num_states + 1, ..., n*num_states + n entries are susceptibles in rural areas 1,...,n
+.
+.
+.
+
 States:
 1 -> S
 2 -> E
@@ -12,6 +23,25 @@ States:
 7 -> Cumulative I_sub
 8 -> Cumulative I_dis
 9 -> Cumulative Dead
+
+Events for each location/county:
+
+1-> Urban transmission
+2-> Rural transmission
+3-> #urban E->A
+4-> rural E->A
+5-> urban E->D
+6-> rural E->D
+7-> urban D->H
+8-> rural D->H
+9-> urban H->R
+10-> rural H->R
+11-> urban D->R
+12-> rural D->R
+13-> urban A->R
+14-> rural A->R
+15-> urban H->death
+16-> rural H->death
 
 """
 n_t = 16 #number of events per location
