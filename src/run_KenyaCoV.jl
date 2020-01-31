5bdd1bde811e37431bd9388dbcfd15699534c845
Samuel <sam055@mac.com>
2020-01-31 13:14:00 +0000

    update

--- src/run_KenyaCoV.jl
+++ src/run_KenyaCoV.jl
@@ -1,16 +1,64 @@
 push!(LOAD_PATH, "/Users/Sam/GitHub/KenyaCoV/src")
-using Plots,Parameters,Distributions
-using KenyaCoV
-#Load data and completely susceptible Population
-u0,P,transport_matrix = model_ingredients_from_data("./src/2009_National_Estimates_of_Rural_and_Urban_Populations_by_County.csv",0.01)
-#This method modifies the parameter set for changing the movement structure
-KenyaCoV.transportstructure_params!(P,0.001,transport_matrix)
-#Then you can modify other parameters
-P.τ = 1/1. #e.g. increased treatment rate
-#Define initial conditions by modifying the completely susceptible population
-u0[30,3,1] += 1#One asymptomatic in Nairobi
+using DifferentialEquations,Plots,DataFrames,Parameters,LinearAlgebra,Distributions,Distributed
+include("kenya_data.jl");
+include("types.jl");
+include("gravity_model.jl");
+ρ = 0.01
+location_matrix = similar(transport_matrix)
 
-#Create a JumpProblem which you can solve --- needs DifferentialEquations module for the solvers
-jump_prob_tl = create_KenyaCoV_prob(u0,(0.,365.),P)
-#Go straight to solution using solver compiled in the KenyaCoV module
-@time sol_tl = solve_KenyaCoV_prob(u0,(0.,60.),P,0.25)
+for i = 1:n,j = 1:n
+    if i != j
+        location_matrix[i,j] = ρ*transport_matrix[i,j]
+    else
+        location_matrix[i,j] = 1-ρ
+    end
+end
+
+
+P = CoVParameters(T = location_matrix,ρ = ρ,β = 2.5/3.6,τ =0.
+                ,Î=zeros(n),N̂=zeros(n),λ_urb=zeros(n),λ_rur = zeros(n) )
+"""
+States:
+1 -> S
+2 -> E
+3 -> I_subclinical
+4 -> I_diseased
+5 -> H(ospitalised)
+6 -> Recovered
+7 -> Cumulative I_sub
+8 -> Cumulative I_dis
+9 -> Cumulative Dead
+"""
+u0 = zeros(Int64,n,9,2) #Array by area, state and urban vs rural
+for i = 1:n
+    u0[i,1,1] = KenyaTbl[:Urban][i]
+    if !ismissing(KenyaTbl[:Rural][i])
+        u0[i,1,2] = KenyaTbl[:Rural][i]
+    end
+end
+N = sum(u0)
+N_urb = [sum(u0[i,:,1]) for i = 1:n]
+N_rural = [sum(u0[i,:,2]) for i = 1:n]
+N̂ = location_matrix*N_urb + N_rural
+
+
+u0[30,4,1] += 100#One exposed in Nairobi
+u = u0[:]
+@time calculate_infection_rates!(u,P)
+P.λ_urb[28]
+include("events.jl");
+prob = DiscreteProblem(u0,(0.0,60.0),P)
+jump_prob = JumpProblem(prob,DirectFW(),jump_urb_trans,
+                                    jump_rural_trans,
+                                    jump_incubation,
+                                    jump_recovery,
+                                    jump_hosp,
+                                    jump_death,
+                                    save_positions=(false,false))
+@time sol = solve(jump_prob,FunctionMap(),saveat = 1.)
+# integ = init(jump_prob,FunctionMap(),saveat = 7.)
+sol[:,end][30,:,1]
+
+CoVensemble_prob = EnsembleProblem(jump_prob)
+# addprocs(3)
+CoVensemble = solve(CoVensemble_prob,FunctionMap(),EnsembleThreads(),trajectories = 100)
