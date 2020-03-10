586d2ff8e85ab0b07f2a39d55e9955a6c01a7371
Rabia  Aziza <58255088+Rabia-git@users.noreply.github.com>
2020-03-10 14:31:32 +0000

    plots

--- contacts/plots_from_sims.jl
+++ contacts/plots_from_sims.jl
@@ -1,15 +1,14 @@
-using Plots,MAT
+using Plots,MAT, StatsPlots
 
-results_folder="./contacts\\results_session10\\"
+results_folder="./contacts\\results_session11\\"
 taup_list=[0.0,0.25,0.5,0.75,0.9]
 data_files=readdir(results_folder)#["sims100_taup0.0_capacity10000.mat","sims100_taup0.25_capacity10000.mat","sims100_taup0.5_capacity10000.mat","sims100_taup0.75_capacity10000.mat","sims100_taup0.9_capacity10000.mat"];
 data_files=[s for s in data_files if endswith(s,".mat")]
 n_traj=10
 
 ## Plotting functions IN NAIROBI
-function ALL_TAUP_cumulative_symptomatics_sumages(taup_list,results_folder,data_files)
-    p=plot(); #title("Final cumulative Infecteds(ID+IQ) 100 sims (sum all ages)");   hold on
-    #Legend=cell(size(taup_list,2),1);
+function ALL_TAUP_cumulative_symptomatics_sumages(taup_list,results_folder,data_files,n_traj)
+    p=plot(title="Final cumulative Infecteds(ID+IQ) "*string(n_traj)*" sims (sum all ages)")
     for i=1:size(taup_list,1)
         file = matopen(results_folder*data_files[i])
         data_per_taup=read(file, "sims")
@@ -18,39 +17,32 @@ function ALL_TAUP_cumulative_symptomatics_sumages(taup_list,results_folder,data_
 
         Id_per_taup_nairobi=[]   # Infecteds=I_D+I_Q
         for j=1:size(data_per_taup,2)
-            #Id_per_taup_nairobi=[Id_per_taup_nairobi sum(data_per_taup[end,j][4,:,8])]
             push!(Id_per_taup_nairobi,sum(data_per_taup[end,j][4,:,8]))
         end
-        #Legend{i}=strcat("taup=",num2str(taup_list(i)));
         plot!(p,Id_per_taup_nairobi, label="taup"*string(taup_list[i]))
     end
-    #hold off
-    #legend(Legend);
     display(p)
     savefig(p,results_folder*"cumulative_symptomatics_allages.png")
 end
-#=
-function ALL_TAUP_cumulative_symptomatics_sumages_BOXPLOT(taup_list,results_folder,data_files)
-    p=figure(); title("Final cumulative Infecteds(ID+IQ) 100 sims (sum all ages)");   hold on
-    Legend=cell(size(taup_list,2),1);
-    data=[];
-    for i=1:size(taup_list,2)
-        data_per_taup=load(data_files(i));
-        data_per_taup=data_per_taup.sims(:,2:end);
+function ALL_TAUP_cumulative_symptomatics_sumages_BOXPLOT(taup_list,results_folder,data_files,n_traj)
+    p=boxplot(title="Final cumulative Infecteds(ID+IQ) "*string(n_traj)*" sims (sum all ages)")
+    data=[]
+    for i=1:size(taup_list,1)
+        file = matopen(results_folder*data_files[i])
+        data_per_taup=read(file, "sims")
+        close(file)
+        data_per_taup=data_per_taup[:,2:end]
 
-        Id_per_taup_nairobi=[];   % Infecteds=I_D+I_Q
+        Id_per_taup_nairobi=[];   # Infecteds=I_D+I_Q
         for j=1:size(data_per_taup,2)
-            Id_per_taup_nairobi=[Id_per_taup_nairobi;  sum(data_per_taup{end,j}(4,:,8))];
+            push!(Id_per_taup_nairobi,sum(data_per_taup[end,j][4,:,8]))
         end
-        Legend{i}=strcat("taup=",num2str(taup_list(i)));
-        data=[data Id_per_taup_nairobi];
+        boxplot!(Id_per_taup_nairobi,label="taup"*string(taup_list[i]))
     end
-    boxplot(data);
-    hold off
-    legend(findobj(gca,'Tag','Box'),Legend);
-    saveas(p,results_folder+"cumulative_symptomatics_allages_BOXPLOT.png");
+    display(p)
+    savefig(p,results_folder*"cumulative_symptomatics_allages_BOXPLOT.png")
 end
-=#
+
 ## Calling the functions , IN NAIROBI
-ALL_TAUP_cumulative_symptomatics_sumages(taup_list,results_folder,data_files)
-#ALL_TAUP_cumulative_symptomatics_sumages_BOXPLOT(taup_list,results_folder,data_files)
+ALL_TAUP_cumulative_symptomatics_sumages(taup_list,results_folder,data_files,n_traj)
+ALL_TAUP_cumulative_symptomatics_sumages_BOXPLOT(taup_list,results_folder,data_files,n_traj)
