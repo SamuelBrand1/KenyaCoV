2c7f77b51de06b259e2e998591a2626bf92b4910
Rabia  Aziza <58255088+Rabia-git@users.noreply.github.com>
2020-03-10 17:05:40 +0000

    plots

--- contacts/plots_from_sims.jl
+++ contacts/plots_from_sims.jl
@@ -42,7 +42,27 @@ function ALL_TAUP_cumulative_symptomatics_sumages_BOXPLOT(taup_list,results_fold
     display(p)
     savefig(p,results_folder*"cumulative_symptomatics_allages_BOXPLOT.png")
 end
+function ALL_TAUP_cumulative_symptomatics_perage_BOXPLOT(taup_list,results_folder,data_files,n_traj)
+    p=boxplot(layout=16)
+    for a=1:16
+        data=[]
+        for i=1:size(taup_list,1)
+            file = matopen(results_folder*data_files[i])
+            data_per_taup=read(file, "sims")
+            close(file)
+            data_per_taup=data_per_taup[:,2:end]
 
+            Id_per_taup_nairobi=[];   # Infecteds=I_D+I_Q
+            for j=1:size(data_per_taup,2)
+                push!(Id_per_taup_nairobi,data_per_taup[end,j][4,a,8])
+            end
+            boxplot!(Id_per_taup_nairobi,label="taup"*string(taup_list[i])*" a"*string(a),subplot=a)
+        end
+    end
+    display(p)
+    savefig(p,results_folder*"cumulative_symptomatics_perage_BOXPLOT.png")
+end
 ## Calling the functions , IN NAIROBI
-ALL_TAUP_cumulative_symptomatics_sumages(taup_list,results_folder,data_files,n_traj)
-ALL_TAUP_cumulative_symptomatics_sumages_BOXPLOT(taup_list,results_folder,data_files,n_traj)
+#ALL_TAUP_cumulative_symptomatics_sumages(taup_list,results_folder,data_files,n_traj)
+#ALL_TAUP_cumulative_symptomatics_sumages_BOXPLOT(taup_list,results_folder,data_files,n_traj)
+ALL_TAUP_cumulative_symptomatics_perage_BOXPLOT(taup_list,results_folder,data_files,n_traj)
