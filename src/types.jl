5ab0fc25d482809591d048f5599d98fa904e555a
Samuel <sam055@mac.com>
2020-04-28 22:44:31 +0100

    Changed h_a to an age dependent vector

--- src/types.jl
+++ src/types.jl
@@ -24,19 +24,27 @@
     dc::SparseMatrixCSC{Int64,Int64} = sparse(zeros(Int64,n*n_s*2,n*n_t))#For inplace calculations
 end
 
+"""
+    mutable struct CoVParameters_AS
+
+Struct for containing relevant epidemilogical parameters for the age-structured version of KenyaCoV
+"""
 @with_kw mutable struct CoVParameters_AS
-    β::Float64 = 1.
+    #Epidemiological parameters and social contact/mixing rates
+    β::Float64 = 1. #Basic transmission probability per contact OR infectious contact rate (can be greater than one depending on scaling)
     c_t::Function = t -> 1. #Time varying basic contact rate
-    γ::Float64 = 1/2.5
-    σ::Float64 = 1/5.
+    γ::Float64 = 1/4. #recovery rate for mild and asymptomatic cases (Consensus estimate mean 4 days)
+    σ₁::Float64 = 1/5. #end of latency rate (Consensus estimate mean 5 days)
+    σ₂::Float64 = 1/2.5 #end of pre-symptomatic rate (Consensus estimate mean 5 days)
     δ::Float64 = 0.9#Proportion of over 80s who get identified
-    τ::Float64 = 0. #current isolation rate for symptomatics
+    τ::Float64 = 1/5. #Rate of hospitalisation treatment, conditional on eventually needing it (V category)
     τ_initial::Float64 = 0. # isolation rate for symptomatics at beginning of epidemic
-    clear_quarantine = 1/14. # Two weeks on average to end isolation
-    μ₁::Float64 = 0.0#Excess mortality due to disease
-    ϵ::Float64 = 0.1 #Relative infectiousness of undetectable infecteds
-    ϵ_D::Float64 = 1.#Relative infectiousness of detectable infecteds after intervention
-    rel_detection_rate::Vector{Float64} = ones(n_a) #relative detection rate
+    clear_quarantine = 0. # Average time to end isolation NOT USED IN THIS VERSION
+    μ₁::Float64 = 0.0#Excess mortality due to disease NOT USED IN THIS VERSION
+    ϵ::Float64 = 0.1 #Relative infectiousness of undetectable/undetected infecteds both pre-symptomatic and asymptomatic
+    ϵ_D::Float64 = 1.#Relative infectiousness of detectable infecteds due to social avoidance
+    rel_detection_rate::Vector{Float64} = ones(n_a) #relative symptomatic rate
+    hₐ::Vector{Float64} = zeros(n_a)   #proportion of severe cases if symptomatic
     χ::Vector{Float64} = ones(n_a) #relative susceptibility
     ρ::Vector{Float64} = [0.01 for i in 1:n] #Time spent outside of area
     T::Matrix{Float64} = zeros(n,n)#Probability distributions of location for mobile individuals
@@ -45,9 +53,11 @@ end
     ext_inf_rate::Float64 = 0. #Scales the contact rate with the infecteds arriving via air
     into_mom::Vector{Int}#Number of people flying into Mombassa each day
     into_nai::Vector{Int}#Number of people flying into Nairobi each day
-    global_prev::Vector{Float64}
-    isolating_detecteds::Bool = true #This determines if people are still being isolated
-    lockdown::Bool = true  #This determines if social distancing and travel restrictions are still in force
+    global_prev::Vector{Float64}#Chance that each person arriving was infected on that day
+    #Control variables
+    isolating_detecteds::Bool = false #This determines if people are still being isolated
+    lockdown::Bool = false  #This determines if social distancing and travel restrictions are still in force
+    #Calculation variables
     dt::Float64 = 1. #Useful for the non-negative method
     Î::Matrix{Float64} = zeros(n_wa,n_a) #For inplace calculations
     N̂::Matrix{Float64} = zeros(n_wa,n_a)#For inplace calculations
@@ -57,7 +67,4 @@ end
     poi_rates::Vector{Float64} = zeros(n_wa*n_a*n_ta)#For inplace calculations
     dc::SparseMatrixCSC{Int64,Int64} = sparse(zeros(Int64,n_wa*n_a*n_s,n_wa*n_a*n_ta))#For inplace calculations
     du_linear::Vector{Int64} = zeros(Int64,n_wa*n_a*n_s) #for inplace calculations
-
-    ## Consensus parameters:
-    hₐ::Float64 = .05   #proportion of severe cases if symptomatic
 end
