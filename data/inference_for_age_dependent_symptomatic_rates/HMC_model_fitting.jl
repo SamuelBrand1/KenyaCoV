d313c429034b3633d24419d32712bd82ab83fe74
Ojal <ojal.johnowino@gmail.com>
2020-05-08 00:52:14 +0300

    model fittings script

---
+++ data/inference_for_age_dependent_symptomatic_rates/HMC_model_fitting.jl
@@ -0,0 +1,7 @@
+# File used for model fitting to the Kenyan data
+
+using TransformVariables, LogDensityProblems, DynamicHMC, DynamicHMC.Diagnostics, Distributions,MAT,Plots,StatsPlots
+using MCMCDiagnostics, LinearAlgebra
+using Parameters, Statistics, Random
+import ForwardDiff
+using CSV,DataFrames,JLD2
