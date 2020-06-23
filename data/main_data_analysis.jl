4a09505ad7ec22fc6f914e63200610c340e44fd8
Samuel Brand <sam055@mac.com>
2020-06-23 15:55:38 +0100

    added data

--- data/main_data_analysis.jl
+++ data/main_data_analysis.jl
@@ -1,5 +1,5 @@
 #Script for building the population pyramid into data
-using DataFrames,Plots,JLD2,MAT,LinearAlgebra,Distances,StatsPlots,CSV
+using DataFrames,Plots,JLD2,MAT,LinearAlgebra,Distances,StatsPlots,CSV,RData
 using LinearAlgebra:normalize,normalize!
 using TransformVariables, DynamicHMC, DynamicHMC.Diagnostics, Distributions
 age_cats = ["0-4","5-9","10-14","15-19","20-24","25-29","30-34","35-39","40-44","45-49","50-54","55-59","60-64","65-69","70-74","75-79","80+"]
